a58546827c780f814b5c36b2c03c26075c72b853
Riza Khan <riza.khan@appnovation.com>
2021-08-19 10:41:01 -0400

    small refactoring

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require("laravel-mix");
 
 mix.ts("resources/js/app.ts", "public/js")
-    .vue({ version: 3 })
-    .postCss("resources/css/app.css", "public/css", [
-        require("postcss-import"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-    ]);
+	.vue({ version: 3 })
+	.postCss("resources/css/app.css", "public/css", [
+		require("postcss-import"),
+		require("tailwindcss"),
+		require("autoprefixer"),
+	]);
