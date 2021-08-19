1b0328c70bec5cee6f4a70dc9da771c6d3999f1e
Riza Khan <riza.khan@appnovation.com>
2021-08-19 00:28:21 -0400

    redirect to dashboard if logged in

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if(!Auth::guest()){
+        return redirect()->route('dashboard');
+    }
     return view('welcome');
 });
 
