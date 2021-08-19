678b94c880fe8c6c430b4ce45f770b5fba351e3c
Riza Khan <riza.khan@appnovation.com>
2021-08-19 14:43:53 -0400

    adding any routes after dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    if(!Auth::guest()){
+    if (!Auth::guest()) {
         return redirect()->route('dashboard');
     }
     return view('welcome');
 });
 
-Route::get('/dashboard/{any?}', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard/{any?}', [DashboardController::class, 'index'])->where('any', '.*')->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
