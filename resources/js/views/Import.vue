596fc26e46c0204f24747be9ef539c895aa018b2
Riza Khan <riza.khan@appnovation.com>
2021-08-20 07:26:57 -0400

    new styling and views

--- resources/js/views/Import.vue
+++ resources/js/views/Import.vue
@@ -1,3 +1,18 @@
 <template>
-	<h1>I am the import component</h1>
+	<h2>Import File:</h2>
+	<form action="/">
+		<label for="file">Choose an Excel File</label>
+		<input type="file" id="file" accept=".xlsx, .xls, .csv" />
+		<button>Upload</button>
+	</form>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	setup() {
+		return {};
+	},
+});
+</script>
