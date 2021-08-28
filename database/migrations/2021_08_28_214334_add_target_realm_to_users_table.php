32046d1d600d54a3d3f5eacad0f3061766ac6388
Riza Khan <riza.khan@appnovation.com>
2021-08-28 17:56:36 -0400

    Renaming column
    [quickbooks]
    []

--- database/migrations/2021_08_28_214334_add_target_realm_to_users_table.php
+++ database/migrations/2021_08_28_214334_add_target_realm_to_users_table.php
@@ -14,7 +14,7 @@ class AddTargetRealmToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('target-realm')->nullable();
+            $table->string('target_realm')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddTargetRealmToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('target-realm');
+            $table->dropColumn('target_realm');
         });
     }
 }
