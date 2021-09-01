86e564841ef672d10439cd0252f43c170bf16ef0
Riza Khan <riza.khan@appnovation.com>
2021-09-01 14:31:52 -0400

    master http://jiralink.com/master

--- resources/js/store/modules/index.js
+++ resources/js/store/modules/index.js
@@ -1,1 +1,22 @@
-export default {};
+import Company from "./models/Company.ts";
+
+const state = () => ({
+	company: {},
+});
+
+const getters = {};
+
+const mutations = {
+	setCompany(state: any, company: any) {
+		state.company = new Company(company);
+	},
+};
+
+const actions = {};
+
+export default {
+	state,
+	getters,
+	mutations,
+	actions,
+};
