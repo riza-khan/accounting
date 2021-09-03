b43aff5778b9d650e79b58177c9c19c7e0c0eb99
Riza Khan <riza.khan@appnovation.com>
2021-09-03 19:23:09 -0400

    created a new modal
    Branch: workflow
    Jira Link: n/a
    Type: Feature

---
+++ resources/js/components/molecules/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="modal">
+		<h1>I am a modal</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	setup() {
+		return {};
+	},
+});
+</script>
+
