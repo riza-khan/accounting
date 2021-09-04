8b6384e4de023afaed71861a7bdea6ef647df086
Riza Khan <riza.khan@appnovation.com>
2021-09-04 09:03:50 -0400

    Some refactoring
    Branch: workflow
    Jira Link: n/a
    Type: Feature

--- resources/js/api/index.ts
+++ resources/js/api/index.ts
@@ -2,24 +2,24 @@ import axios from "axios";
 import store from "../store";
 
 const Axios = axios.create({
-	baseURL: process.env.API_URL,
-	headers: { Accept: "application/json" },
+    baseURL: process.env.API_URL,
+    headers: { Accept: "application/json" },
 });
 
 Axios.interceptors.request.use(
-	(config) => {
-		store.commit("setLoader", true);
-		return config;
-	},
-	(error) => Promise.reject(error)
+    (config) => {
+        store.commit("setLoader", true);
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
 
 Axios.interceptors.response.use(
-	(response) => {
-		store.commit("setLoader", false);
-		return response;
-	},
-	(error) => Promise.reject(error)
+    (response) => {
+        store.commit("setLoader", false);
+        return response;
+    },
+    (error) => Promise.reject(error)
 );
 
 export default Axios;
