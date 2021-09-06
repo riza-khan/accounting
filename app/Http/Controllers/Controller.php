1fa8fdaf0c4edc6967a40462bfb4650777e5c7cb
Riza Khan <riza.khan@appnovation.com>
2021-09-06 00:16:03 -0400

    moving setup of data service to root controller
    Branch: workflow
    Jira Link: n/a
    Type: Feature

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use QuickBooksOnline\API\DataService\DataService;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function dataService()
+    {
+        return DataService::Configure(array(
+            'auth_mode'       => 'oauth2',
+            'ClientID'        => env('QUICKBOOKS_CLIENT_ID'),
+            'ClientSecret'    => env('QUICKBOOKS_CLIENT_SECRET'),
+            'accessTokenKey'  => Auth::user()->access_token_key,
+            'refreshTokenKey' => Auth::user()->access_token_secret,
+            'QBORealmID'      => Auth::user()->target_realm,
+            'baseUrl'         => "Development"
+        ));
+    }
 }
