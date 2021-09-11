baf1265f64b1cea82b604fae2b8858a10021da07
Riza Khan <riza.khan@appnovation.com>
2021-09-11 19:33:56 -0400

    Removing used methods
    Branch: workflow
    Jira Link: n/a
    Type: Feature

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -11,22 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ImportController extends Controller
 {
-    public function store(Request $request)
-    {
-        $file = $request->file('file')->store('import');
-
-        (new UsersImport)->import($file);
-        return back()->withStatus('Excel file imported successfully');
-    }
-
-    public function cars(Request $request): Response
-    {
-        $file = $request->file('file')->store('import');
-
-        (new CarsImport)->import($file);
-        return response('File successfully imported', 200);
-    }
-
     public function invoices(Request $request): Response
     {
         $file = $request->file('file')->store('import');
