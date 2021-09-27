7ff2ad8a3fd5a7377ed64a62e7e56b50726459ad
Riza Khan <riza.khan@appnovation.com>
2021-09-27 13:25:29 -0400

    More types added
    Branch: workflow
    Jira Link: n/a
    Type: Feature

--- resources/js/store/index.ts
+++ resources/js/store/index.ts
@@ -1,48 +1,70 @@
 import { createStore } from "vuex";
 import Axios from "../api";
 
+interface Company {
+	[key: string]: any;
+}
+
+interface ModalTargetObj {
+	[key: string]: any;
+}
+
+interface Category {
+	[key: string]: any;
+}
+
+interface State {
+	loading: boolean;
+	token: string;
+	company: Company;
+	categories: Category[];
+	modalTargetObj: ModalTargetObj;
+	loader: boolean;
+}
+
 export default createStore({
-    state: {
-        loading: false,
-        token: "",
-        company: {},
-        categories: [],
-        modalTargetObj: {},
-        loader: false,
-    },
-    getters: {
-        getLoading: (state: any): boolean => state.loading,
-        getToken: (state: any): string => state.token,
-        getCompany: (state: any): any => state.company,
-        getCompanyName: (state: any): any => state.company.CompanyName,
-        getCategories: (state: any): any => state.categories,
-        getLoader: (state: any): any => state.loader,
-        getModalTargetObj: (state: any): any => state.modalTargetObj,
-    },
-    mutations: {
-        setToken(state, token) {
-            state.token = token;
-        },
-        setModalTargetObj(state, obj) {
-            state.modalTargetObj = obj;
-        },
-        setCompany(state, company) {
-            state.company = company;
-        },
-        setCategories(state, categories) {
-            state.categories = categories;
-        },
-        setLoader(state, newState) {
-            state.loader = newState;
-        },
-        logout() {
-            Axios.post("/logout");
-        },
-    },
-    actions: {
-        async getCategories({ commit }) {
-            const categories = await Axios.get("/api/categories");
-            commit("setCategories", categories.data);
-        },
-    },
+	state: {
+		loading: false,
+		token: "",
+		company: {},
+		categories: [],
+		modalTargetObj: {},
+		loader: false,
+	},
+	getters: {
+		getLoading: (state: State): boolean => state.loading,
+		getToken: (state: State): string => state.token,
+		getCompany: (state: State): State.company => state.company,
+		getCompanyName: (state: State): string => state.company.CompanyName,
+		getCategories: (state: State): State.categories => state.categories,
+		getLoader: (state: State): boolean => state.loader,
+		getModalTargetObj: (state: State): State.modalTargetObj =>
+			state.modalTargetObj,
+	},
+	mutations: {
+		setToken(state, token) {
+			state.token = token;
+		},
+		setModalTargetObj(state, obj) {
+			state.modalTargetObj = obj;
+		},
+		setCompany(state, company) {
+			state.company = company;
+		},
+		setCategories(state, categories) {
+			state.categories = categories;
+		},
+		setLoader(state, newState) {
+			state.loader = newState;
+		},
+		logout() {
+			Axios.post("/logout");
+		},
+	},
+	actions: {
+		async getCategories({ commit }) {
+			const categories = await Axios.get("/api/categories");
+			commit("setCategories", categories.data);
+		},
+	},
 });
